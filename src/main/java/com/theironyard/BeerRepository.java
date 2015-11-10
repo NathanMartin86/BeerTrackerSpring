dbb5a5a0a0834e8bad82fbca1191d289758ab1fa
macbookair <nj.martin123@gmail.com>
2015-11-10 12:29:04 -0500

    Initial Import

---
+++ src/main/java/com/theironyard/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.theironyard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by macbookair on 11/10/15.
+ */
+public interface BeerRepository extends CrudRepository<Beer,Integer> {
+
+}
