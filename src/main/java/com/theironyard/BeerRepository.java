f5cf0a409c9129b458efd9efe1fc0d93af8ae16a
macbookair <nj.martin123@gmail.com>
2015-11-11 11:22:08 -0500

    Added Advanced Query Methods

--- src/main/java/com/theironyard/BeerRepository.java
+++ src/main/java/com/theironyard/BeerRepository.java
@@ -1,10 +1,24 @@
 package com.theironyard;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by macbookair on 11/10/15.
  */
 public interface BeerRepository extends CrudRepository<Beer,Integer> {
+    List<Beer> findByType(String type);
+    List <Beer> findByTypeAndCaloriesIsLessThanEqual (String type, Integer calories);
+
+    Beer findFirstByType (String type);
+    int countByType (String type);
+    List<Beer>findByTypeOrderByNameAsc(String type);
+
+    @Query("SELECT b FROM Beer b  WHERE LOWER(name) LIKE '%' || LOWER (?)|| '%'")
+    List<Beer>searchByName(String name);
+
+
 
 }
